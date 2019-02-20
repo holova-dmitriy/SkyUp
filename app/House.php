de85fbbc810d8ee16cbbc26db70f7dd02eabfe53
Holova Dmitriy <holova.dmitriy@gmail.com>
2019-02-20 17:36:39 +0200

    Create model

---
+++ app/House.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class House extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'price',
+        'bedrooms',
+        'bathrooms',
+        'storeys',
+        'garages',
+    ];
+}
